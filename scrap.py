06d43ccc0fb6a64e93303d2f4d51530c8300e72f
vigneshkannan255 <32855922+vigneshkannan255@users.noreply.github.com>
2019-07-20 18:55:46 +0530

    Add files via upload
    Scraping the webpage using python2 program 
    using two packages Beautifulsoup and urllib

---
+++ scrap.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup as bs
+import urllib3
+ 
+
+f = open('a.txt','a')
+
+http = urllib3.PoolManager()
+list =[]
+def fun(urll):
+    r = http.request('GET',urll)
+    soup = bs(r.data , 'html.parser')
+
+    for link in soup.find_all('a'):
+   # print(link.get('href'))
+        f.write(link.get('href')+"\n")
+        list.append(link.get('href'))
+        print(link.get('href'))
+
+
+
+
+
+user = raw_input("Enter URL:")
+
+fun(user)
+#for fu in list:
+    #fun(fu)
+    #print(fu)
